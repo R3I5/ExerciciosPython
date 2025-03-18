62bfda08f1cbb03ad8e06b942f58c1da1b0f89da
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-18 12:21:41 -0300

    Teste

---
+++ Exercicios-Basico/Exercicio002.py
@@ -0,0 +1,4 @@
+##Faça um programa que peça um número e então mostre a mensagem "O numero informado foi{numero}"
+
+numero = int(input("Insira um número"))
+print(f"O numero informado foi:{numero}")
--- NivelBasico/input.py
+++
@@ -1,4 +0,0 @@
-## Trabalhando com entrada e saída de dados
-
-nome = input("Digite seu nome:")
-print(f"Olá, {nome}! Seja bem-vindo ao nosso programa.")
