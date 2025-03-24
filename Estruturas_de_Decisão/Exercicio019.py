5f3b04229b96bc75a68eb0eb89685484d0171687
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-24 09:27:23 -0300

    Create Exercicio019.py

---
+++ Estruturas_de_Decisão/Exercicio019.py
@@ -0,0 +1,9 @@
+#Faça um programa que peça dois números e imprima o maior deles
+
+numero1 = float(input("Insira o primeiro número:"))
+numero2 = float(input("Insira o segundo número:"))
+
+if numero1>numero2:
+    print("O primeiro número é maior que o segundo")
+else:
+    print("O segundo número é maior que o primeiro")
