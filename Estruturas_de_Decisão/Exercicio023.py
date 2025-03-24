ac947bc02780a5c8a4a33f13db34b8eb2f21adc5
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-24 11:37:20 -0300

    Dia 5

---
+++ Estruturas_de_Decisão/Exercicio023.py
@@ -0,0 +1,3 @@
+#Faça um programa para a leiturua de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar: A mensagem "Aprovado", se a média alcançada for maior ou igual a sete
+#A mensagem reprovado se a média for menor do que sete: A mensagem "Aprovado com distinção" se a média for igual a dez
+
