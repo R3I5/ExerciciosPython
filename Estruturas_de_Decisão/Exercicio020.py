e88cad9a3f1dcd2f396d5584d768789ce6ad148c
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-24 09:30:03 -0300

    Create Exercicio020.py

---
+++ Estruturas_de_Decisão/Exercicio020.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça um valor e mostre na tela se  o valor é positivo ou negativoo
+
+valor = float(input("Insira o valor desejado:"))
+if valor >= 0:
+    print("O valor é positivo")
+else:
+    print("O valor é negativo")
