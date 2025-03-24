bdec0c6cdd56e24f34040ba1a5c25a4bf9de9e7c
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-24 09:07:45 -0300

    PrimeiraParteLista

---
+++ Estruturas_Sequenciais/Exercicio018.py
@@ -0,0 +1,3 @@
+#Faça um programa que peça o tamanho de um arquivo para dowload (em MB) e a velocidade de um link da Internet(em Mbps)
+#calcule e informe o tempo aproximado de download do arquivo usando este link(em minutos)
+
