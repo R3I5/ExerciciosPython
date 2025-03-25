ed974e2e24c6ad132c8edd5c362ad73910233df7
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-25 10:48:12 -0300

    Create exercicio 024

---
+++ Estruturas_de_Decisão/Exercicio024.py
@@ -0,0 +1,12 @@
+#Faça um programa que leia três números e mostre o maior deles
+
+numero1 = float(input("Insira o primeiro numero:"))
+numero2 = float(input("Insira o segundo numero:"))
+numero3 = float(input("Insira o terceiro numero:"))
+
+if numero1>numero2 and numero3:
+    print("O maior numero é o primeiro")
+elif numero2>numero3 and umero1:
+    print("O maior numero é o segundo")
+elif numero3>numero2 and numero1:
+    print("O maior numero é o terceiro")
