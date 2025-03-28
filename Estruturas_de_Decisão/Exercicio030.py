7dcc53e036a613c2874e714c4505a468c7e1b363
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-28 12:11:44 -0300

    Exercicio 30

---
+++ Estruturas_de_Decisão/Exercicio030.py
@@ -0,0 +1,2 @@
+# Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, que depende do salário bruto(conforme tabela abaixo) e 10% para o inss e que o fgts corresponde a 11% do salário bruto, mas não é descontado
+
