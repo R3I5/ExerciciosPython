e75130cd5fb912762b0cc8329f5893c5e2c3f9bf
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-28 10:29:58 -0300

    Exercicio 25 Revisado

--- Estruturas_de_Decisão/Exercicio025.py
+++ Estruturas_de_Decisão/Exercicio025.py
@@ -13,9 +13,9 @@ def maior(numero1, numero2, numero3):
         return numero3
         
 def menor(numero1, numero2, numero3):
-    if numero1<numero2 and numero3:
+    if numero1<numero2 and numero1< numero3:
         return numero1
-    elif numero2<numero1 and numero3:
+    elif numero2<numero1 and numero2< numero3:
         return numero2
     else:
         return numero3
