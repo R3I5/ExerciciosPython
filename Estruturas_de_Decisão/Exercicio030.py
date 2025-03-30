6f5324b74fa181bc1600cd968506804b4870d073
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-29 23:13:12 -0300

    Update Exercicio 30

--- Estruturas_de_Decisão/Exercicio030.py
+++ Estruturas_de_Decisão/Exercicio030.py
@@ -1,2 +1,24 @@
 # Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, que depende do salário bruto(conforme tabela abaixo) e 10% para o inss e que o fgts corresponde a 11% do salário bruto, mas não é descontado
 
+horas = float(input('Insira quantas horas são trabalhadas: '))
+valorHora = float(input('Insira o valor por hora: '))
+salarioBruto = horas * valorHora
+impostoRenda = 0
+if salarioBruto <= 900:
+    impostoRenda = 0
+elif 900 < salarioBruto <= 1500:
+    impostoRenda = (salarioBruto*0.05)
+elif 1500 < salarioBruto <= 2500:
+    impostoRenda = (salarioBruto*0.1)
+else:
+    impostoRenda = (salarioBruto * 0.2)
+INSS = (0.1 * salarioBruto)
+FGTS = (0.11 * salarioBruto)
+descontosTotais = INSS + impostoRenda
+salarioLiquido = salarioBruto - descontosTotais
+print(f'Salario Bruto: ({horas} * {valorHora}: R$ {salarioBruto:.2f})')
+print(f'(-) IR (5%): R$ {impostoRenda:.2f}')
+print(f'(-) INSS (10%): R$ {INSS:.2f}')
+print(f'FGTS (11%): R$ {FGTS:.2f}')
+print(f'Total de descontos: R$ {descontosTotais:.2f}')
+print(f'Salário Liquído: R$ {salarioLiquido:.2f}')
