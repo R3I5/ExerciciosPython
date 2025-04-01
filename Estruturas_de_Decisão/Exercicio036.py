da6de19fb05ca87633c0c6c7057c39360800d310
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-01 10:20:48 -0300

    Create Exercicio 36

--- Estruturas_de_Decisão/Exercicio036.py
+++ Estruturas_de_Decisão/Exercicio036.py
@@ -1,2 +1,24 @@
-# Faça um programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida
+data = input('Digite uma data no formato dd/mm/aaaa: ')
+dia, mes, ano = data.split("/")
+dia, mes, ano = int(dia), int(mes), int(ano)
 
+if ano < 0:
+    print('Ano inválido!')
+elif mes < 1 or mes > 12:
+    print('Mês inválido!')
+else:
+    # Definição do número de dias de cada mês
+    if mes in [1, 3, 5, 7, 8, 10, 12]:
+        max_dias = 31
+    elif mes in [4, 6, 9, 11]:
+        max_dias = 30
+    else:  # Fevereiro
+        if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):  # Ano bissexto
+            max_dias = 29
+        else:
+            max_dias = 28
+
+    if 1 <= dia <= max_dias:
+        print('Data válida!')
+    else:
+        print('Dia inválido!')
