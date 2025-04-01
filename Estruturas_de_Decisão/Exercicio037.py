c1005c2e43456cfd7191e03a46c5fc68cb891237
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-01 11:07:34 -0300

    Create Exercicio 37

---
+++ Estruturas_de_Decisão/Exercicio037.py
@@ -0,0 +1,13 @@
+#Faça um programa que leia um número inteiro maior que 0 e menor que 1000 e imprima a quantidade de centenas, dezenas e unidades do mesmo.
+# Observando os termos no plural a colocação do 'e', da vírgula entre outros
+
+numero = float(input('Insira o número desejado: '))
+if 0 > numero or numero >= 1000:
+    print('Número inválido')
+else:
+    unidade = numero % 10
+    dezena = (numero % 100) // 10
+    centena = numero // 100
+    separador1 = ""
+    separador2 = ""
+    
